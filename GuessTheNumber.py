35caf09c81b9f718a71f1e9b26ea7c2dd744bdf5
NibirChowdhury <36859458+NibirChowdhury@users.noreply.github.com>
2022-11-29 23:07:29 +0600

    Add files via upload
    It's a simple math game where a user has to think of a number and have and should follow the instruction accordingly, and in the end, the bot will surprise the user by telling him his remaining number.

---
+++ GuessTheNumber.py
@@ -0,0 +1,26 @@
+"""
+1. Tell him to multiply the chosen number by 2.
+2. Choose an even number to use yourself. Ask your friend to add this number to the one in their head.
+3. Tell them to divide the new number by 2.
+4. Tell them to subtract their original number from the equation.
+    "Guess" the number."""
+import msvcrt as m
+import random
+def wait():
+    m.getch()
+
+print("Remind a numbe and press enter")
+wait() 
+print("Multiplay it with 2 and press enter")
+wait() 
+number = ((random.randint(0,99)*2))
+print("I give you ", number," to add and press enter")
+wait()
+print("Now devide it in half and press enter")
+wait()
+print("Take back your number and press enter")
+wait()
+result = number/2
+print("You have ", result, " remail")
+
+
